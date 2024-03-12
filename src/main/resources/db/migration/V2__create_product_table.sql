8386f07315f291f6c7bef825a7b9e0a92df35b4b
Adauto Moisés <adautomaleandro@gmail.com>
2024-03-12 11:36:47 -0300

    first commit

---
+++ src/main/resources/db/migration/V2__create_product_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE products.product (
+    id BIGSERIAL PRIMARY KEY,
+    product_identifier VARCHAR NOT NULL,
+    nome VARCHAR(100) NOT NULL,
+    descricao VARCHAR NOT NULL,
+    preco FLOAT NOT NULL,
+    category_id BIGINT REFERENCES products.category(id)
+);
---
+++ src/main/resources/db/migration/V3__insert_categories.sql
@@ -0,0 +1,4 @@
+INSERT INTO products.category(id, nome)
+VALUES (1, 'Eletrônico'),
+       (2, 'Móveis'),
+       (3, 'Brinquedos');
