7cb2489e04cd171f80a7c722d3789389a1dab940
Adauto Moisés <adautomaleandro@gmail.com>
2024-03-14 14:14:05 -0300

    Renomeando a rota de product

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -11,7 +11,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/produto")
+@RequestMapping("/product")
 @RequiredArgsConstructor
 public class ProductController {
     private final ProductService productService;
